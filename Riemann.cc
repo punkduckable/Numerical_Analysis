8618528df82cd820d083c8ae9826aba076456b2e
Robert Stephany <robert.stephany@utexas.edu>
2018-12-19 09:29:43 -0600

    Multiple accumulators
    I redid the Left and Right Riemann sum codes to use 4 accumulators. I believe this code runs quicker (though I am unsure because of the function pointer... idek).

--- Riemann.cc
+++ Riemann.cc
@@ -16,19 +16,42 @@ double Numerical::Integration::Riemann::Left(double (*F)(double), const double a
 
   //////////////////////////////////////////////////////////////////////////////
   // Integration
+  // The Left Riemann approximation is,
+  //                                Left Approx = Sum(F(x_i)*h)
 
-  /* Now we can perform the Left Riemann approximation,
-                                  Left Approx = Sum(F(x_i)*h)
-  But, since the node with, h, is the same for every node, we can pull it out
+  // Calculate h.
+  double h = (b-a)/(double)n;
+
+  // Since addition commutes, I can use multiple accumulators to improve runtime.
+  double Integral;
+  double Integral0 = 0;
+  double Integral1 = 0;
+  double Integral2 = 0;
+  double Integral3 = 0;
+
+  /* Further, since the node with, h, is the same for every node, we can pull it out
   of the summation. Therefore,
                                   Left Approx = h*Sum(F(x_i))
   This eliminates n-1 multiplications (we only have to do one multiplication
   rather than one for each step). */
-  double Integral = 0;
-  double h = (b-a)/(double)n;
-  for(int i = 0; i < n+1; i++)
-    Integral += F(x[i]);
 
+  // Main loop
+  int i;
+  for(i = 0; i < n - 3; i += 4) {
+    Integral0 += F(x[i]);
+    Integral1 += F(x[i+1]);
+    Integral2 += F(x[i+2]);
+    Integral3 += F(x[i+3]);
+  } // for(int i = 0; i < (n+1) - 3; i += 4) {
+
+  // Clean-up loop
+  for( ; i < n+1; i++)
+    Integral0 += F(x[i]);
+
+  // Combine the multiple accumulators.
+  Integral = Integral0 + Integral1 + Integral2 + Integral3;
+
+  // Now multiply by h.
   Integral *= h;
 
   // Free nodes
@@ -55,21 +78,45 @@ double Numerical::Integration::Riemann::Right(double (*F)(double), const double 
 
   //////////////////////////////////////////////////////////////////////////////
   // Integration
+  // The Right Riemann approximation is,
+  //                                Right Approx = Sum(F(x_i+1)*h)
 
-  /* Now we can perform the Right Riemann approximation,
-                                  Right Approx = Sum(F(x_i+1)*h)
-  But, since the node with, h, is the same for every node, we can pull it out
+  // Calculate h.
+  double h = (b-a)/(double)n;
+
+
+  // Since addition commutes, I can use multiple accumulators to improve runtime.
+  double Integral;
+  double Integral0 = 0;
+  double Integral1 = 0;
+  double Integral2 = 0;
+  double Integral3 = 0;
+
+
+  /* Further, since the node with, h, is the same for every node, we can pull it out
   of the summation. Therefore,
                                   Right Approx = h*Sum(F(x_i+1))
   This eliminates n-1 multiplications (we only have to do one multiplication
   rather than one for each step). */
-  double Integral = 0;
-  double h = (b-a)/(double)n;
-  for(int i = 0; i < n+1; i++)
-    Integral += F(x[i+1]);
 
+  // Main loopp
+  int i;
+  for(i = 0; i < n - 3; i += 4) {
+    Integral0 += F(x[i+1]);
+    Integral1 += F(x[i+3]);
+    Integral2 += F(x[i+3]);
+    Integral3 += F(x[i+4]);
+  } // for(i = 0; i < n - 3; i += 4) {
+
+  // Clean up loop
+  for( ; i < n; i++)
+    Integral0 += F(x[i+1]);
+
+  // Combine the multiple accumulators
+  Integral = Integral0 + Integral1 + Integral2 + Integral3;
+
+  // Now multiply through by h
   Integral *= h;
-
 
   // Free the nodes
   delete [] x;
